0289b6d184b9d8a25cb5fb7dc1dd2319f735c716
Dhrumil <24ec150@charusat.edu.in>
2026-09-15 20:55:38 +0530

    Update code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 function calculateResult(a: number, b: number, c: number) {
     const unusedValue = 123;
 
-    if (a > 0) {
-        if (b > 0) {
+    if (a > 0) {        if (b > 0) {
             if (c > 0) {
                 return a + b + c;
             } else {
