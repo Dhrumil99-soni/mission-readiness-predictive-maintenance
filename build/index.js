70b57a60fc62300d7732d149abc011bfa9064988
Dhrumil <24ec150@charusat.edu.in>
2026-09-15 21:19:16 +0530

    add build files

---
+++ build/index.js
@@ -0,0 +1,30 @@
+function calculateResult(a, b, c) {
+    const unusedValue = 123;
+    if (a > 0) {
+        if (b > 0) {
+            if (c > 0) {
+                return a + b + c;
+            }
+            else {
+                return a + b;
+            }
+        }
+        else {
+            if (c > 0) {
+                return a + c;
+            }
+            else {
+                return a;
+            }
+        }
+    }
+    else {
+        if (b > 0) {
+            return b + c;
+        }
+        else {
+            return c;
+        }
+    }
+}
+export {};
